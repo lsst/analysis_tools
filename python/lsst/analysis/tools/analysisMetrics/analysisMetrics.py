296c75fdec2130d2f263bc6d7f3fb1093229a275
Yusra AlSayyad <yusra@astro.princeton.edu>
2022-06-10 14:01:05 -0500

    Add MagDiff VectorAction and WPerp notes

--- python/lsst/analysis/tools/analysisMetrics/analysisMetrics.py
+++ python/lsst/analysis/tools/analysisMetrics/analysisMetrics.py
@@ -1,10 +1,13 @@
 from __future__ import annotations
-
+from ..analysisParts.base import BasePrep
 from ..analysisParts.shapeSizeFractional import (
     ShapeSizeFractionalPrep,
     ShapeSizeFractionalProcess,
     ShapeSizeFractionalPostProcessMetric,
 )
+from ..analysisParts.wPerp import WPerpProcess, WPerpPostProcessMetric
+# in the future this will be
+# from ..analysisMetrics.base import BaseMetric
 from ..interfaces import AnalysisMetric
 
 
@@ -13,3 +16,10 @@ class ShapeSizeFractionalMetric(AnalysisMetric):
         self.prep = ShapeSizeFractionalPrep()
         self.process = ShapeSizeFractionalProcess()
         self.post_process = ShapeSizeFractionalPostProcessMetric()
+
+
+class WPerpMetric(AnalysisMetric):
+    def setDefaults(self):
+        super().setDefaults()
+        self.process = WPerpProcess()
+        self.post_process = WPerpPostProcessMetric()
--- python/lsst/analysis/tools/vectorActions/vectorActions.py
+++ python/lsst/analysis/tools/vectorActions/vectorActions.py
@@ -2,12 +2,15 @@ from __future__ import annotations
 
 from typing import cast
 
+from astropy import units as u
 import numpy as np
-from lsst.pex.config import Field
-from lsst.pipe.tasks.configurableActions import ConfigurableActionField
+from lsst.pex.config import Field, DictField
+from lsst.pipe.tasks.configurableActions import ConfigurableActionField,
 
 from ..interfaces import KeyedData, KeyedDataSchema, Vector, VectorAction
 
+
+_LOG = logging.getLogger(__name__)
 
 class MagColumnNanoJansky(VectorAction):
     columnKey = Field(doc="column key to use for this transformation", dtype=str)
@@ -48,3 +51,102 @@ class LoadVector(VectorAction):
 
     def __call__(self, data: KeyedData, **kwargs) -> Vector:
         return cast(Vector, data[cast(str, self.vectorKey).format(**kwargs)])
+
+
+class MagDiff(VectorAction):
+    """Calculate the difference between two magnitudes;
+    each magnitude is derived from a flux column.
+    Parameters
+    ----------
+    TO DO:
+    Returns
+    -------
+    The magnitude difference in milli mags.
+    Notes
+    -----
+    The flux columns need to be in units (specifiable in
+    the fluxUnits1 and 2 config options) that can be converted
+    to janskies. This action doesn't have any calibration
+    information and assumes that the fluxes are already
+    calibrated.
+    """
+
+    col1 = Field(doc="Column to subtract from", dtype=str)
+    fluxUnits1 = Field(doc="Units for col1", dtype=str, default="nanojansky")
+    col2 = Field(doc="Column to subtract", dtype=str)
+    fluxUnits2 = Field(doc="Units for col2", dtype=str, default="nanojansky")
+    returnMillimags = Field(doc="Use millimags or not?", dtype=bool, default=True)
+
+    def getInputSchema(self, **kwargs) -> KeyedDataSchema:
+        return ((self.col1.format(**kwargs), Vector), (self.col2.format(**kwargs), Vector))
+
+    def __call__(self, data: KeyedData, **kwargs) -> Vector:
+        flux1 = data[self.col1.format(**kwargs)] * u.Unit(self.fluxUnits1)
+        mag1 = flux1.to(u.ABmag)
+
+        flux2 = data[self.col2.format(**kwargs)] * u.Unit(self.fluxUnits2)
+        mag2 = flux2.to(u.ABmag)
+
+        magDiff = mag1 - mag2
+
+        if self.returnMillimags:
+            magDiff = magDiff.to(u.mmag)
+
+        return magDiff
+
+
+class ExtinctionCorrectedMagDiff(VectorAction):
+    """Compute the difference between two magnitudes and correct for extinction
+    By default bands are derived from the <band>_ prefix on flux columns,
+    per the naming convention in the Object Table:
+    e.g. the band of 'g_psfFlux' is 'g'. If column names follow another
+    convention, bands can alternatively be supplied via the band1 or band2
+    config parameters.
+    If band1 and band2 are supplied, the flux column names are ignored.
+    """
+
+    magDiff = ConfigurableActionField(doc="Action that returns a difference in magnitudes",
+                                          default=MagDiff, dtype=VectorAction)
+    ebvCol = Field(doc="E(B-V) Column Name", dtype=str, default="ebv")
+    band1 = Field(doc="Optional band for magDiff.col1. Supercedes column name prefix",
+                  dtype=str, optional=True, default=None)
+    band2 = Field(doc="Optional band for magDiff.col2. Supercedes column name prefix",
+                  dtype=str, optional=True, default=None)
+    extinctionCoeffs = DictField(
+        doc="Dictionary of extinction coefficients for conversion from E(B-V) to extinction, A_band."
+            "Key must be the band",
+        keytype=str, itemtype=float, optional=True,
+        default=None)
+
+    @property
+    def columns(self):
+        return self.magDiff.columns + (self.ebvCol,)
+
+    def getInputSchema(self, **kwargs) -> KeyedDataSchema:
+        return self.magDiff.getInputSchema(**kwargs) + ((self.ebvCol.format(**kwargs), Vector), )
+
+    def __call__(self, df):
+        diff = self.magDiff(df)
+        if not self.extinctionCoeffs:
+            _LOG.warning("No extinction Coefficients. Not applying extinction correction")
+            return diff
+
+        col1Band = self.band1 if self.band1 else self.magDiff.col1.split('_')[0]
+        col2Band = self.band2 if self.band2 else self.magDiff.col2.split('_')[0]
+
+        for band in (col1Band, col1Band):
+            if band not in self.extinctionCoeffs:
+                _LOG.warning("%s band not found in coefficients dictionary: %s"
+                             " Not applying extinction correction", band, self.extinctionCoeffs)
+                return diff
+
+        av1 = self.extinctionCoeffs[col1Band]
+        av2 = self.extinctionCoeffs[col2Band]
+
+        ebv = df[self.ebvCol].values
+        correction = (av1 - av2) * ebv * u.mag
+
+        if self.magDiff.returnMillimags:
+            correction = correction.to(u.mmag)
+
+        return diff - correction

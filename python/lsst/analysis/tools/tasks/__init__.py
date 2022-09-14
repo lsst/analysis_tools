f6718172a67c9dbb06ac0a1ca9cde924e7a119fd
Eric Bellm <ecbellm@uw.edu>
2022-09-14 11:12:36 -0700

    draft SSO metric

--- python/lsst/analysis/tools/tasks/__init__.py
+++ python/lsst/analysis/tools/tasks/__init__.py
@@ -1,4 +1,5 @@
 from .associatedSourcesTractAnalysis import *
 from .catalogMatch import *
+from .diaSourceTableVisitAnalysis import *
 from .objectTableTractAnalysis import *
 from .sourceTableVisitAnalysis import *
---
+++ python/lsst/analysis/tools/tasks/diaSourceTableVisitAnalysis.py
@@ -0,0 +1,31 @@
+from __future__ import annotations
+
+from lsst.pipe.base import connectionTypes as ct
+
+from .base import AnalysisBaseConfig, AnalysisBaseConnections, AnalysisPipelineTask
+
+
+class DiaSourceTableCcdVisitAnalysisConnections(
+    AnalysisBaseConnections,
+    dimensions=("visit", "band"),
+    defaultTemplates={"coaddName": "deep", "fakesType": "fakes_"},
+):
+
+    data = ct.Input(
+        doc="CcdVisit-based DiaSource table to load from the butler",
+        name="{fakesType}{coaddName}Diff_assocDiaSrc",
+        storageClass="DataFrame",
+        dimensions=("visit", "band", "detector"),
+        deferLoad=True,
+    )
+
+
+class DiaSourceTableCcdVisitAnalysisConfig(
+    AnalysisBaseConfig, pipelineConnections=DiaSourceTableCcdVisitAnalysisConnections
+):
+    pass
+
+
+class DiaSourceTableCcdVisitAnalysisTask(AnalysisPipelineTask):
+    ConfigClass = DiaSourceTableCcdVisitAnalysisConfig
+    _DefaultName = "DiaSourceTableCcdVisitAnalysis"

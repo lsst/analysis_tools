45d1c17fe033d9fb885ae95b8bc68557b2677a88
Arun Kannawadi <arunkannawadi@astro.princeton.edu>
2022-12-01 09:20:07 -0800

    Add a new survey analysis task

--- python/lsst/analysis/tools/tasks/__init__.py
+++ python/lsst/analysis/tools/tasks/__init__.py
@@ -3,5 +3,6 @@ from .catalogMatch import *
 from .diaObjectTableAssocAnalysis import *
 from .diaSourceTableVisitAnalysis import *
 from .diffMatchedAnalysis import *
+from .objectTableSurveyAnalysis import *
 from .objectTableTractAnalysis import *
 from .sourceTableVisitAnalysis import *

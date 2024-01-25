0c71257c472ab976edeb93981c814e070bcc0f1c
Lee Kelvin <lkelvin@astro.princeton.edu>
2024-01-25 06:26:37 -0800

    Add missing __all__ to deltaSkyCorrAnalysis

--- python/lsst/analysis/tools/tasks/deltaSkyCorrAnalysis.py
+++ python/lsst/analysis/tools/tasks/deltaSkyCorrAnalysis.py
@@ -18,6 +18,16 @@
 #
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+__all__ = (
+    "DeltaSkyCorrHistTask",
+    "DeltaSkyCorrHistConfig",
+    "DeltaSkyCorrHistConnections",
+    "DeltaSkyCorrAnalysisConnections",
+    "DeltaSkyCorrAnalysisConfig",
+    "DeltaSkyCorrAnalysisTask",
+)
+
 import logging
 
 import numpy as np

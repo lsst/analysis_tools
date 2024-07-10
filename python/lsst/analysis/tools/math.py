52681301b68f047b19c1095c33804a82bd346a6d
Dan Taranu <dtaranu@astro.princeton.edu>
2024-07-09 19:15:45 -0700

    Add numpy_invalid_value_scalar_divide

--- python/lsst/analysis/tools/math.py
+++ python/lsst/analysis/tools/math.py
@@ -55,6 +55,7 @@ from .warning_control import (
     numpy_invalid_value_divide,
     numpy_invalid_value_log,
     numpy_invalid_value_log10,
+    numpy_invalid_value_scalar_divide,
     numpy_invalid_value_sin,
     numpy_invalid_value_sqrt,
     numpy_invalid_value_subtract,
@@ -75,6 +76,7 @@ def divide(dividend: Scalar | Vector, divisor: Scalar | Vector) -> Scalar | Vect
     with warnings.catch_warnings():
         warnings.filterwarnings(filterwarnings_action, numpy_divide_zero_divide)
         warnings.filterwarnings(filterwarnings_action, numpy_invalid_value_divide)
+        warnings.filterwarnings(filterwarnings_action, numpy_invalid_value_scalar_divide)
         result = dividend / divisor
     return result
 
--- python/lsst/analysis/tools/warning_control.py
+++ python/lsst/analysis/tools/warning_control.py
@@ -31,9 +31,10 @@ __all__ = (
     "numpy_invalid_value_divide",
     "numpy_invalid_value_log",
     "numpy_invalid_value_log10",
-    "numpy_invalid_value_subtract",
+    "numpy_invalid_value_scalar_divide",
     "numpy_invalid_value_sin",
     "numpy_invalid_value_sqrt",
+    "numpy_invalid_value_subtract",
     "numpy_mean_empty",
 )
 
@@ -51,7 +52,8 @@ numpy_invalid_value_cos = r"invalid value encountered in cos"
 numpy_invalid_value_divide = r"invalid value encountered in divide"
 numpy_invalid_value_log = r"invalid value encountered in log"
 numpy_invalid_value_log10 = r"invalid value encountered in log10"
-numpy_invalid_value_subtract = r"invalid value encountered in subtract"
+numpy_invalid_value_scalar_divide = r"invalid value encountered in scalar divide"
 numpy_invalid_value_sin = r"invalid value encountered in sin"
 numpy_invalid_value_sqrt = r"invalid value encountered in sqrt"
+numpy_invalid_value_subtract = r"invalid value encountered in subtract"
 numpy_mean_empty = r"Mean of empty slice"

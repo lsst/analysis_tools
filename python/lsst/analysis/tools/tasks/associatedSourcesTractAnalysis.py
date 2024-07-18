ba937a999b3affb03680228a65d276a59e9436d7
Clare Saunders <cmsaunders@princeton.edu>
2024-07-18 08:56:30 -0700

    Add proper motion/parallax correction to astrometry metrics

--- python/lsst/analysis/tools/tasks/associatedSourcesTractAnalysis.py
+++ python/lsst/analysis/tools/tasks/associatedSourcesTractAnalysis.py
@@ -22,12 +22,18 @@ from __future__ import annotations
 
 __all__ = ("AssociatedSourcesTractAnalysisConfig", "AssociatedSourcesTractAnalysisTask")
 
+import astropy.time
+import astropy.units as u
+import lsst.pex.config as pexConfig
 import numpy as np
-from astropy.table import join, vstack
+import pandas as pd
+from astropy.table import Table, join, vstack
+from lsst.drp.tasks.gbdesAstrometricFit import calculate_apparent_motion
 from lsst.geom import Box2D
 from lsst.pipe.base import NoWorkFound
 from lsst.pipe.base import connectionTypes as ct
 from lsst.skymap import BaseSkyMap
+from smatch import Matcher
 
 from ..interfaces import AnalysisBaseConfig, AnalysisBaseConnections, AnalysisPipelineTask
 
@@ -70,11 +76,59 @@ class AssociatedSourcesTractAnalysisConnections(
         dimensions=("instrument",),
         isCalibration=True,
     )
+    astrometricCorrectionCatalogs = ct.Input(
+        doc="Catalog containing proper motion and parallax.",
+        name="gbdesAstrometricFit_starCatalog",
+        storageClass="ArrowNumpyDict",
+        dimensions=("instrument", "skymap", "tract", "physical_filter"),
+        multiple=True,
+        deferLoad=True,
+    )
+    visitTable = ct.Input(
+        doc="Catalog containing visit information.",
+        name="visitTable",
+        storageClass="DataFrame",
+        dimensions=("instrument",),
+    )
+
+    def __init__(self, *, config=None):
+        super().__init__(config=config)
+
+        if not config.applyAstrometricCorrections:
+            self.inputs.remove("astrometricCorrectionCatalogs")
+            self.inputs.remove("visitTable")
 
 
 class AssociatedSourcesTractAnalysisConfig(
     AnalysisBaseConfig, pipelineConnections=AssociatedSourcesTractAnalysisConnections
 ):
+    applyAstrometricCorrections = pexConfig.Field(
+        dtype=bool,
+        default=True,
+        doc="Apply proper motion and parallax corrections to source positions.",
+    )
+    matchingRadius = pexConfig.Field(
+        dtype=float,
+        default=0.2,
+        doc=(
+            "Radius in mas with which to match the mean positions of the sources with the positions in the"
+            " astrometricCorrectionCatalogs."
+        ),
+    )
+    astrometricCorrectionParameters = pexConfig.DictField(
+        keytype=str,
+        itemtype=str,
+        # TODO: DM-45845 Update default names when catalog gets updated.
+        default={
+            "ra": "starX",
+            "dec": "starY",
+            "pmRA": "starPMx",
+            "pmDec": "starPMy",
+            "parallax": "starParallax",
+        },
+        doc="Column names for position and motion parameters in the astrometric correction catalogs.",
+    )
+
     def setDefaults(self):
         super().setDefaults()
 
@@ -91,28 +145,39 @@ class AssociatedSourcesTractAnalysisTask(AnalysisPipelineTask):
         tractBox = tractInfo.getBBox()
         return tractBox, wcs
 
-    @classmethod
-    def callback(cls, inputs, dataId):
+    def callback(self, inputs, dataId):
         """Callback function to be used with reconstructor."""
-        return cls.prepareAssociatedSources(
+        return self.prepareAssociatedSources(
             inputs["skyMap"],
             dataId["tract"],
             inputs["sourceCatalogs"],
             inputs["associatedSources"],
+            inputs["astrometricCorrectionCatalogs"],
+            inputs["visitTable"],
         )
 
-    @classmethod
-    def prepareAssociatedSources(cls, skymap, tract, sourceCatalogs, associatedSources):
+    def prepareAssociatedSources(
+        self,
+        skymap,
+        tract,
+        sourceCatalogs,
+        associatedSources,
+        astrometricCorrectionCatalogs=None,
+        visitTable=None,
+    ):
         """Concatenate source catalogs and join on associated object index."""
 
         # Keep only sources with associations
         sourceCatalogStack = vstack(sourceCatalogs)
         dataJoined = join(sourceCatalogStack, associatedSources, keys="sourceId", join_type="inner")
 
+        if astrometricCorrectionCatalogs is not None:
+            self.applyAstrometricCorrections(dataJoined, astrometricCorrectionCatalogs, visitTable)
+
         # Determine which sources are contained in tract
         ra = np.radians(dataJoined["coord_ra"])
         dec = np.radians(dataJoined["coord_dec"])
-        box, wcs = cls.getBoxWcs(skymap, tract)
+        box, wcs = self.getBoxWcs(skymap, tract)
         box = Box2D(box)
         x, y = wcs.skyToPixelArray(ra, dec)
         boxSelection = box.contains(x, y)
@@ -122,6 +187,77 @@ class AssociatedSourcesTractAnalysisTask(AnalysisPipelineTask):
         dataFiltered = dataJoined[boxSelection]
 
         return dataFiltered
+
+    def applyAstrometricCorrections(self, dataJoined, astrometricCorrectionCatalogs, visitTable):
+        """Use proper motion/parallax catalogs to shift positions to median
+        epoch of the visits.
+
+        Parameters
+        ----------
+        dataJoined : `astropy.table.Table`
+            Table containing source positions, which will be modified in place.
+        astrometricCorrectionCatalogs: `dict` [`pd.DataFrame`]
+            Dictionary keyed by band with proper motion and parallax catalogs.
+        visitTable : `pd.DataFrame`
+            Table containing the MJDs of the visits.
+        """
+        for band in np.unique(dataJoined["band"]):
+            bandInd = dataJoined["band"] == band
+            bandSources = dataJoined[bandInd]
+            # Add key for sorting below.
+            bandSources["__index__"] = np.arange(len(bandSources))
+            bandSourcesDf = bandSources.to_pandas()
+            meanRAs = bandSourcesDf.groupby("obj_index")["coord_ra"].aggregate("mean")
+            meanDecs = bandSourcesDf.groupby("obj_index")["coord_dec"].aggregate("mean")
+
+            bandPMs = astrometricCorrectionCatalogs[band]
+            with Matcher(meanRAs, meanDecs) as m:
+                idx, i1, i2, d = m.query_radius(
+                    bandPMs[self.config.astrometricCorrectionParameters["ra"]],
+                    bandPMs[self.config.astrometricCorrectionParameters["dec"]],
+                    (self.config.matchingRadius * u.mas).to(u.degree),
+                    return_indices=True,
+                )
+
+            catRAs = np.zeros_like(meanRAs)
+            catDecs = np.zeros_like(meanRAs)
+            pmRAs = np.zeros_like(meanRAs)
+            pmDecs = np.zeros_like(meanRAs)
+            parallaxes = np.zeros(len(meanRAs))
+            catRAs[i1] = bandPMs[self.config.astrometricCorrectionParameters["ra"]][i2]
+            catDecs[i1] = bandPMs[self.config.astrometricCorrectionParameters["dec"]][i2]
+            pmRAs[i1] = bandPMs[self.config.astrometricCorrectionParameters["pmRA"]][i2]
+            pmDecs[i1] = bandPMs[self.config.astrometricCorrectionParameters["pmDec"]][i2]
+            parallaxes[i1] = bandPMs[self.config.astrometricCorrectionParameters["parallax"]][i2]
+
+            pmDf = Table(
+                {
+                    "ra": catRAs * u.degree,
+                    "dec": catDecs * u.degree,
+                    "pmRA": pmRAs * u.mas / u.yr,
+                    "pmDec": pmDecs * u.mas / u.yr,
+                    "parallax": parallaxes * u.mas,
+                    "obj_index": meanRAs.index,
+                }
+            )
+
+            dataWithPM = join(bandSources, pmDf, keys="obj_index", join_type="left")
+
+            visits = bandSourcesDf["visit"].unique()
+            mjds = [visitTable.loc[visit]["expMidptMJD"] for visit in visits]
+            mjdTable = Table(
+                [astropy.time.Time(mjds, format="mjd", scale="tai"), visits], names=["MJD", "visit"]
+            )
+            dataWithMJD = join(dataWithPM, mjdTable, keys="visit", join_type="left")
+            # After astropy 7.0, it should be possible to use "keep_order=True"
+            # in the join and avoid sorting.
+            dataWithMJD.sort("__index__")
+            medianMJD = astropy.time.Time(np.median(mjds), format="mjd", scale="tai")
+
+            raCorrection, decCorrection = calculate_apparent_motion(dataWithMJD, medianMJD)
+
+            dataJoined["coord_ra"][bandInd] = dataWithMJD["coord_ra"] - raCorrection.value
+            dataJoined["coord_dec"][bandInd] = dataWithMJD["coord_dec"] - decCorrection.value
 
     def runQuantum(self, butlerQC, inputRefs, outputRefs):
         inputs = butlerQC.get(inputRefs)
@@ -134,6 +270,18 @@ class AssociatedSourcesTractAnalysisTask(AnalysisPipelineTask):
         for handle in inputs["sourceCatalogs"]:
             sourceCatalogs.append(self.loadData(handle, names))
         inputs["sourceCatalogs"] = sourceCatalogs
+
+        if self.config.applyAstrometricCorrections:
+            astrometricCorrections = {}
+            for pmCatRef in inputs["astrometricCorrectionCatalogs"]:
+                pmCat = pmCatRef.get(
+                    parameters={"columns": self.config.astrometricCorrectionParameters.values()}
+                )
+                astrometricCorrections[pmCatRef.dataId["band"]] = pd.DataFrame(pmCat)
+            inputs["astrometricCorrectionCatalogs"] = astrometricCorrections
+        else:
+            inputs["astrometricCorrectionCatalogs"] = None
+            inputs["visitTable"] = None
 
         dataId = butlerQC.quantum.dataId
         plotInfo = self.parsePlotInfo(inputs, dataId, connectionName="associatedSources")
--- python/lsst/analysis/tools/tasks/sourceObjectTableAnalysis.py
+++ python/lsst/analysis/tools/tasks/sourceObjectTableAnalysis.py
@@ -20,17 +20,125 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 from __future__ import annotations
 
-__all__ = ("SourceObjectTableAnalysisConfig", "SourceObjectTableAnalysisTask")
+__all__ = (
+    "SourceObjectTableAnalysisConfig",
+    "SourceObjectTableAnalysisTask",
+    "ObjectEpochTableConfig",
+    "ObjectEpochTableTask",
+)
 
+import astropy.time
+import astropy.units as u
 import lsst.pex.config as pexConfig
+import lsst.pipe.base as pipeBase
 import numpy as np
 import pandas as pd
-from astropy.table import vstack
-from lsst.pipe.base import NoWorkFound
+from astropy.table import Table, vstack
+from lsst.drp.tasks.gbdesAstrometricFit import calculate_apparent_motion
 from lsst.pipe.base import connectionTypes as ct
 from smatch import Matcher
 
 from ..interfaces import AnalysisBaseConfig, AnalysisBaseConnections, AnalysisPipelineTask
+
+
+class ObjectEpochTableConnections(
+    pipeBase.PipelineTaskConnections,
+    dimensions=("tract", "skymap"),
+):
+    objectCat = ct.Input(
+        doc="Catalog of positions in each patch.",
+        name="objectTable",
+        storageClass="DataFrame",
+        dimensions=["skymap", "tract", "patch"],
+        multiple=True,
+        deferLoad=True,
+        deferGraphConstraint=True,
+    )
+
+    epochMap = ct.Input(
+        doc="Healsparse map of mean epoch of objectCat in each band.",
+        name="deepCoadd_epoch_map_mean",
+        storageClass="HealSparseMap",
+        dimensions=("skymap", "tract", "band"),
+        multiple=True,
+        deferLoad=True,
+    )
+
+    objectEpochs = ct.Output(
+        doc="Catalog of epochs for objectCat objects.",
+        name="object_epoch",
+        storageClass="ArrowAstropy",
+        dimensions=["skymap", "tract", "patch"],
+        multiple=True,
+    )
+
+
+class ObjectEpochTableConfig(pipeBase.PipelineTaskConfig, pipelineConnections=ObjectEpochTableConnections):
+    bands = pexConfig.ListField(
+        doc=("Bands in objectCat to be combined with `objectCat_selectors` to build objectCat column names."),
+        dtype=str,
+        default=["u", "g", "r", "i", "z", "y"],
+    )
+
+
+class ObjectEpochTableTask(pipeBase.PipelineTask):
+    """Collect mean epochs for the observations that went into each object.
+
+    TODO: DM-46202, Remove this task once the object epochs are available
+    elsewhere.
+    """
+
+    ConfigClass = ObjectEpochTableConfig
+    _DefaultName = "objectEpochTable"
+
+    def getEpochs(self, cat, epochMapDict):
+        """Get mean epoch of the visits corresponding to object position.
+
+        Parameters
+        ----------
+        cat : `pd.DataFrame`
+            Catalog containing object positions.
+        epochMapDict: `dict` [`DeferredDatasetHandle`]
+            Dictionary of handles for healsparse maps containing the mean epoch
+            for positions in the reference catalog.
+
+        Returns
+        -------
+        epochDf = `pd.DataFrame`
+            Catalog with mean epoch of visits at each object position.
+        """
+        allEpochs = {}
+        for band in self.config.bands:
+            epochs = np.ones(len(cat)) * np.nan
+            validPositions = np.isfinite(cat[f"{band}_ra"]) & np.isfinite(cat[f"{band}_dec"])
+            if validPositions.any():
+                bandEpochs = epochMapDict[band].get_values_pos(
+                    cat[f"{band}_ra"][validPositions], cat[f"{band}_dec"][validPositions]
+                )
+                epochsValid = epochMapDict[band].get_values_pos(
+                    cat[f"{band}_ra"][validPositions], cat[f"{band}_dec"][validPositions], valid_mask=True
+                )
+                bandEpochs[~epochsValid] = np.nan
+                epochs[validPositions] = bandEpochs
+            allEpochs[f"{band}_epoch"] = epochs
+        allEpochs["objectId"] = cat.index
+
+        epochTable = Table(allEpochs)
+        return epochTable
+
+    def runQuantum(self, butlerQC, inputRefs, outputRefs):
+        inputs = butlerQC.get(inputRefs)
+
+        columns = [f"{band}_{coord}" for band in self.config.bands for coord in ["ra", "dec"]]
+
+        inputs["epochMap"] = {ref.dataId["band"]: ref.get() for ref in inputs["epochMap"]}
+
+        outputEpochRefs = {outputRef.dataId["patch"]: outputRef for outputRef in outputRefs.objectEpochs}
+        for objectCatRef in inputs["objectCat"]:
+            patch = objectCatRef.dataId["patch"]
+            objectCat = objectCatRef.get(parameters={"columns": columns})
+            epochs = self.getEpochs(objectCat, inputs["epochMap"])
+            butlerQC.put(epochs, outputEpochRefs[patch])
 
 
 class SourceObjectTableAnalysisConnections(
@@ -47,7 +155,7 @@ class SourceObjectTableAnalysisConnections(
         doc="Visit based source table to load from the butler",
         name="sourceTable_visit",
         storageClass="ArrowAstropy",
-        dimensions=("visit", "band"),
+        dimensions=("visit",),
         deferLoad=True,
     )
 
@@ -70,6 +178,36 @@ class SourceObjectTableAnalysisConnections(
         deferLoad=True,
         deferGraphConstraint=True,
     )
+    astrometricCorrectionCatalog = ct.Input(
+        doc="Catalog containing proper motions and parallaxes.",
+        name="gbdesAstrometricFit_starCatalog",
+        storageClass="ArrowNumpyDict",
+        dimensions=("instrument", "skymap", "tract", "physical_filter"),
+        multiple=True,
+        deferLoad=True,
+    )
+    refCatEpochs = ct.Input(
+        doc="Catalog of epochs for refCat objects.",
+        name="object_epoch",
+        storageClass="ArrowAstropy",
+        dimensions=["skymap", "tract", "patch"],
+        multiple=True,
+        deferLoad=True,
+    )
+    visitTable = ct.Input(
+        doc="Catalog containing visit information.",
+        name="visitTable",
+        storageClass="DataFrame",
+        dimensions=("instrument",),
+    )
+
+    def __init__(self, *, config=None):
+        super().__init__(config=config)
+
+        if not config.applyAstrometricCorrections:
+            self.inputs.remove("astrometricCorrectionCatalog")
+            self.inputs.remove("refCatEpochs")
+            self.inputs.remove("visitTable")
 
 
 class SourceObjectTableAnalysisConfig(
@@ -85,6 +223,15 @@ class SourceObjectTableAnalysisConfig(
         dtype=str,
         default="r_dec",
     )
+    epoch_column = pexConfig.Field(
+        doc=(
+            "Name of column in refCat corresponding to the epoch to which "
+            "sources will be shifted. Should correspond to the positions in "
+            "`ra_column` and `dec_column`."
+        ),
+        dtype=str,
+        default="r_epoch",
+    )
     refCat_bands = pexConfig.ListField(
         doc=("Bands in refCat to be combined with `refCat_selectors` to build refCat column names."),
         dtype=str,
@@ -98,14 +245,173 @@ class SourceObjectTableAnalysisConfig(
         dtype=str,
         default=["pixelFlags_saturated", "pixelFlags_saturatedCenter"],
     )
+    refCatMatchingRadius = pexConfig.Field(
+        dtype=float,
+        default=1.0,
+        doc=(
+            "Radius in mas with which to match the mean positions of the sources with the positions in the"
+            " reference catalog."
+        ),
+    )
+    applyAstrometricCorrections = pexConfig.Field(
+        dtype=bool,
+        default=True,
+        doc="Apply proper motions and parallaxes to source positions.",
+    )
+    correctionsMatchingRadius = pexConfig.Field(
+        dtype=float,
+        default=0.2,
+        doc=(
+            "Radius in mas with which to match the mean positions of the sources with the positions in the"
+            " astrometricCorrectionCatalog."
+        ),
+    )
+    astrometricCorrectionParameters = pexConfig.DictField(
+        keytype=str,
+        itemtype=str,
+        # TODO: DM-45845 Change default names.
+        default={
+            "ra": "starX",
+            "dec": "starY",
+            "pmRA": "starPMx",
+            "pmDec": "starPMy",
+            "parallax": "starParallax",
+        },
+        doc="Column names for position and motion parameters in the astrometric correction catalogs.",
+    )
 
 
 class SourceObjectTableAnalysisTask(AnalysisPipelineTask):
     ConfigClass = SourceObjectTableAnalysisConfig
     _DefaultName = "sourceObjectTableAnalysis"
 
+    def callback(self, inputs, dataId):
+        """Callback function to be used with reconstructor."""
+        return self.prepareAssociatedSources(
+            dataId["visit"],
+            inputs["data"],
+            inputs["associatedSources"],
+            inputs["refCat"],
+            inputs["visitTable"],
+            inputs["astrometricCorrectionCatalog"],
+        )
+
+    def applyAstrometricCorrections(
+        self, isolatedSources, astrometricCorrectionCatalog, visitTable, visit, refEpochs
+    ):
+        """Shift source positions to match the epoch of the reference catalog
+        objects.
+
+        Parameters
+        ----------
+        isolatedSources : `astropy.table.Table`
+            Catalog of sources which will be modified in place with the
+            astrometric corrections.
+        astrometricCorrectionCatalog : `pd.DataFrame`
+            Catalog with proper motion and parallax information.
+        visitTable : `pd.DataFrame`
+            Catalog containing the epoch for the visit corresponding to the
+            isolatedSources.
+        visit : `int`
+            Identifier of the isolatedSources' visit.
+        """
+        sourceMjd = visitTable.loc[visit]["expMidptMJD"]
+
+        # Get target date from reference catalog
+        targetEpochs = refEpochs.to_numpy()
+        # There may not be a valid reference epoch on the edge of a given
+        # region. Do not make an astrometric correction for any sources on the
+        # edge.
+        targetEpochs[~np.isfinite(targetEpochs)] = sourceMjd
+
+        with Matcher(isolatedSources["coord_ra"], isolatedSources["coord_dec"]) as m:
+            idx, i1, i2, d = m.query_radius(
+                astrometricCorrectionCatalog[self.config.astrometricCorrectionParameters["ra"]],
+                astrometricCorrectionCatalog[self.config.astrometricCorrectionParameters["dec"]],
+                self.config.correctionsMatchingRadius / 3600,
+                return_indices=True,
+            )
+
+        params = self.config.astrometricCorrectionParameters
+        pmDf = Table(
+            {
+                "ra": astrometricCorrectionCatalog[params["ra"]].to_numpy() * u.degree,
+                "dec": astrometricCorrectionCatalog[params["dec"]].to_numpy() * u.degree,
+                "pmRA": astrometricCorrectionCatalog[params["pmRA"]].to_numpy() * u.mas / u.yr,
+                "pmDec": astrometricCorrectionCatalog[params["pmDec"]].to_numpy() * u.mas / u.yr,
+                "parallax": astrometricCorrectionCatalog[params["parallax"]].to_numpy() * u.mas,
+            }
+        )
+
+        pmDf["MJD"] = astropy.time.Time(sourceMjd, format="mjd", scale="tai")
+        raCorrection, decCorrection = calculate_apparent_motion(
+            pmDf[i2], astropy.time.Time(targetEpochs[i1], format="mjd", scale="tai")
+        )
+
+        isolatedSources["coord_ra"][i1] -= raCorrection.value
+        isolatedSources["coord_dec"][i1] -= decCorrection.value
+
+    def prepareAssociatedSources(
+        self, visit, data, associatedSourceRefs, refCats, visitTable, astrometricCorrectionCatalog
+    ):
+        """Match isolated sources with reference objects and shift the sources
+        to the object epochs if `self.config.applyAstrometricCorrections` is
+        True.
+
+        Parameters
+        ----------
+        visit : `int`
+            Identifier of the visit corresponding to the data.
+        data : `astropy.table.Table`
+            Catalog of sources to be associated.
+        associatedSourceRefs : `list` [`DeferredDatasetHandle`]
+            Handle for the catalogs of isolated sources. There will be multiple
+            if the visit overlaps with multiple tracts.
+        refCats : `list` [`pd.DataFrame`]
+            Catalog of objects with which the sources will be compared.
+        visitTable : `pd.DataFrame`
+            Catalog containing the epoch for the visit corresponding to the
+            isolatedSources.
+        astrometricCorrectionCatalog : `pd.DataFrame`
+            Catalog with proper motion and parallax information.
+        """
+        isolatedSources = []
+        for associatedSourceRef in associatedSourceRefs:
+            associatedSources = associatedSourceRef.get(parameters={"columns": ["visit", "source_row"]})
+            visit_sources = associatedSources[associatedSources["visit"] == visit]
+            isolatedSources.append(data[visit_sources["source_row"]])
+        isolatedSources = vstack(isolatedSources)
+
+        if len(isolatedSources) == 0:
+            raise pipeBase.NoWorkFound(f"No isolated sources found for visit {visit}")
+
+        with Matcher(isolatedSources["coord_ra"], isolatedSources["coord_dec"]) as m:
+            idx, isolatedMatchIndices, refMatchIndices, dists = m.query_radius(
+                refCats[self.config.ra_column].values,
+                refCats[self.config.dec_column].values,
+                self.config.refCatMatchingRadius / 3600.0,
+                return_indices=True,
+            )
+
+        matchIS = isolatedSources[isolatedMatchIndices]
+        # Apply proper motions and parallaxes to visit sources.
+        if self.config.applyAstrometricCorrections:
+            refCatEpochs = refCats[self.config.epoch_column].iloc[refMatchIndices]
+            self.applyAstrometricCorrections(
+                matchIS, astrometricCorrectionCatalog, visitTable, visit, refCatEpochs
+            )
+
+        matchRef = refCats.iloc[refMatchIndices]
+        matchIS = matchIS.to_pandas()
+
+        allCat = pd.concat([matchRef.reset_index(), matchIS.reset_index()], axis=1)
+        return allCat
+
     def runQuantum(self, butlerQC, inputRefs, outputRefs):
         inputs = butlerQC.get(inputRefs)
+
+        dataId = butlerQC.quantum.dataId
+        plotInfo = self.parsePlotInfo(inputs, dataId)
 
         # Get isolated sources:
         visit = inputs["data"].dataId["visit"]
@@ -113,20 +419,12 @@ class SourceObjectTableAnalysisTask(AnalysisPipelineTask):
         names = self.collectInputNames()
         names -= {self.config.ra_column, self.config.dec_column}
         data = inputs["data"].get(parameters={"columns": names})
+        inputs["data"] = data
 
-        dataId = butlerQC.quantum.dataId
-        plotInfo = self.parsePlotInfo(inputs, dataId)
-
-        isolatedSources = []
-        for associatedSourcesRef in inputs["associatedSources"]:
-            associatedSources = associatedSourcesRef.get(parameters={"columns": ["visit", "source_row"]})
-            visit_sources = associatedSources[associatedSources["visit"] == visit]
-            isolatedSources.append(data[visit_sources["source_row"]])
-        isolatedSources = vstack(isolatedSources)
-
-        if len(isolatedSources) == 0:
-            raise NoWorkFound(f"No isolated sources found for visit {visit}")
-
+        if self.config.applyAstrometricCorrections:
+            refCatEpochs = {
+                epochTable.dataId["patch"]: epochTable.get() for epochTable in inputs["refCatEpochs"]
+            }
         # Get objects:
         allRefCats = []
         refCatSelectors = [
@@ -142,6 +440,8 @@ class SourceObjectTableAnalysisTask(AnalysisPipelineTask):
                     + refCatSelectors
                 }
             )
+            if self.config.applyAstrometricCorrections:
+                refCat = pd.merge(refCat, refCatEpochs[refCatRef.dataId["patch"]].to_pandas(), on="objectId")
             goodInds = (
                 refCat["detect_isPrimary"]
                 & np.isfinite(refCat[self.config.ra_column])
@@ -151,21 +451,23 @@ class SourceObjectTableAnalysisTask(AnalysisPipelineTask):
             allRefCats.append(refCat[goodInds])
 
         refCat = pd.concat(allRefCats)
-
+        inputs["refCat"] = refCat
         if len(refCat) == 0:
-            raise NoWorkFound(f"No reference catalog objects found to associate with visit {visit}")
+            raise pipeBase.NoWorkFound(f"No reference catalog objects found to associate with visit {visit}")
 
-        with Matcher(isolatedSources["coord_ra"], isolatedSources["coord_dec"]) as m:
-            idx, isolatedMatchIndices, refMatchIndices, dists = m.query_radius(
-                refCat[self.config.ra_column].values,
-                refCat[self.config.dec_column].values,
-                1 / 3600.0,
-                return_indices=True,
-            )
+        if self.config.applyAstrometricCorrections:
+            pmCats = []
+            for astrometricCorrectionCatalogRef in inputs["astrometricCorrectionCatalog"]:
+                pmCat = astrometricCorrectionCatalogRef.get(
+                    parameters={"columns": self.config.astrometricCorrectionParameters.values()}
+                )
+                pmCats.append(pd.DataFrame(pmCat))
+            inputs["astrometricCorrectionCatalog"] = pd.concat(pmCats)
+        else:
+            inputs["astrometricCorrectionCatalog"] = None
+            inputs["visitTable"] = None
 
-        matchRef = refCat.iloc[refMatchIndices]
-        matchIS = isolatedSources[isolatedMatchIndices].to_pandas()
+        allCat = self.callback(inputs, dataId)
 
-        allCat = pd.concat([matchRef.reset_index(), matchIS.reset_index()], axis=1)
         outputs = self.run(data=allCat, bands=band, plotInfo=plotInfo)
         butlerQC.put(outputs, outputRefs)

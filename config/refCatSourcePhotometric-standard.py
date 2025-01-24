cbd817e116c121478dc3410a381f1de39f9eaf15
Jim Bosch <jbosch@astro.princeton.edu>
2025-01-24 12:58:18 -0500

    Factor visit analysis configuration into config files.

---
+++ config/refCatSourcePhotometric-standard.py
@@ -0,0 +1,15 @@
+# Configuration for standard instances of
+# sst.analysis.tools.tasks.refCatSourceAnalysis.RefCatSourceAnalysisTask
+#
+# These configurations are not in the class default configs because there may
+# be instances of the class that are intended to optionally run alongside one
+# that includes this standard set.
+
+from lsst.analysis.tools.atools import *
+
+config.atools.photomDiffPsfSkyVisitPlot = TargetRefCatDeltaPsfSkyVisitPlot
+config.atools.photomDiffAp09SkyVisitPlot = TargetRefCatDeltaAp09SkyVisitPlot
+config.atools.photoDiffPsfScatterVisitPlot = TargetRefCatDeltaPsfScatterVisitPlot
+config.atools.photoDiffCModelScatterVisitPlot = TargetRefCatDeltaAp09ScatterVisitPlot
+
+# TODO: should there be a VisitContext set here, too?
---
+++ config/sourceTable-standard.py
@@ -0,0 +1,15 @@
+# Configuration for standard instances of
+# lsst.analysis.tools.tasks.SourceTableVisitAnalysisTask
+#
+# These configurations are not in the class default configs because there may
+# be instances of the class that are intended to optionally run alongside one
+# that includes this standard set.
+
+from lsst.analysis.tools.atools import *
+from lsst.analysis.tools.contexts import VisitContext
+
+config.atools.skyFluxVisitStatisticMetric = SkyFluxStatisticMetric
+config.atools.skyFluxVisitStatisticMetric.applyContext = VisitContext
+config.atools.skySourceSky = SkySourceSkyPlot
+config.atools.skySourceFlux = SkySourceHistPlot
+config.atools.relativeSizeResidualPlot = RelativeSizeResidualPlot

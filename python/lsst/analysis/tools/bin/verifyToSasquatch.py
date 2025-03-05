7f9ce9be6c44f6db5027ffa279d80c1cd98d1699
BrunoSanchez <bsanchez@cppm.in2p3.fr>
2025-03-05 10:05:25 -0800

    Move AppendDict from verifyToSasquatch to lsst.utils

--- python/lsst/analysis/tools/bin/verifyToSasquatch.py
+++ python/lsst/analysis/tools/bin/verifyToSasquatch.py
@@ -24,7 +24,6 @@ __all__ = [
 ]
 
 import argparse
-import copy
 import datetime
 import logging
 from collections import defaultdict
@@ -34,6 +33,7 @@ import lsst.verify
 from lsst.analysis.tools.interfaces import MetricMeasurementBundle
 from lsst.analysis.tools.interfaces.datastore import SasquatchDispatcher
 from lsst.daf.butler import Butler, DataCoordinate, DatasetRef
+from lsst.utils.argparsing import AppendDict
 
 logging.basicConfig()
 _LOG = logging.getLogger(__name__)
@@ -82,7 +82,7 @@ def makeParser():
     )
     parser.add_argument(
         "--extra",
-        action=_AppendDict,
+        action=AppendDict,
         help="Extra field (in the form key=value) to be added to any records "
         "uploaded to Sasquatch. See SasquatchDispatcher.dispatch and "
         ".dispatchRef for more details. The --extra argument can be passed "
@@ -104,61 +104,6 @@ def makeParser():
     api_group.add_argument("--token", default="na", help="Authentication token for the proxy server.")
 
     return parser
-
-
-class _AppendDict(argparse.Action):
-    """An action analogous to the build-in 'append' that appends to a `dict`
-    instead of a `list`.
-
-    Inputs are assumed to be strings in the form "key=value"; any input that
-    does not contain exactly one "=" character is invalid. If the default value
-    is non-empty, the default key-value pairs may be overwritten by values from
-    the command line.
-    """
-
-    def __init__(
-        self,
-        option_strings,
-        dest,
-        nargs=None,
-        const=None,
-        default=None,
-        type=None,
-        choices=None,
-        required=False,
-        help=None,
-        metavar=None,
-    ):
-        if default is None:
-            default = {}
-        if not isinstance(default, Mapping):
-            argname = option_strings if option_strings else metavar if metavar else dest
-            raise TypeError(f"Default for {argname} must be a mapping or None, got {default!r}.")
-        super().__init__(option_strings, dest, nargs, const, default, type, choices, required, help, metavar)
-
-    def __call__(self, parser, namespace, values, option_string=None):
-        # argparse doesn't make defensive copies, so namespace.dest may be
-        # the same object as self.default. Do the copy ourselves and avoid
-        # modifying the object previously in namespace.dest.
-        mapping = copy.copy(getattr(namespace, self.dest))
-
-        # Sometimes values is a copy of default instead of an input???
-        if isinstance(values, Mapping):
-            mapping.update(values)
-        else:
-            # values may be either a string or list of strings, depending on
-            # nargs. Unsafe to test for Sequence, because a scalar string
-            # passes.
-            if not isinstance(values, list):
-                values = [values]
-            for value in values:
-                vars = value.split("=")
-                if len(vars) != 2:
-                    raise ValueError(f"Argument {value!r} does not match format 'key=value'.")
-                mapping[vars[0]] = vars[1]
-
-        # Other half of the defensive copy.
-        setattr(namespace, self.dest, mapping)
 
 
 def _bundle_metrics(

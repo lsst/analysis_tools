cabf369472c864cb7cad5c4d394996fbc80e9fc0
BrunoSanchez <bsanchez@cppm.in2p3.fr>
2025-09-18 08:15:47 -0700

    Add colormap for rotation angle

--- python/lsst/analysis/tools/tasks/diffimKernelQuiverPlotVisit.py
+++ python/lsst/analysis/tools/tasks/diffimKernelQuiverPlotVisit.py
@@ -24,13 +24,14 @@ __all__ = (
     "MakeDiffimKernelQuiverPlotVisitTask",
 )
 
-
 import lsst.pex.config
 import lsst.pipe.base as pipeBase
-# import matplotlib.cm as cm
 import matplotlib.pyplot as plt
 import numpy as np
+from lsst.obs.lsst import LsstCam
 from lsst.pipe.base import connectionTypes
+
+# from ..actions.plot.plotUtils import addPlotInfo
 
 
 class MakeDiffimKernelQuiverPlotVisitConnections(
@@ -49,7 +50,7 @@ class MakeDiffimKernelQuiverPlotVisitConnections(
         doc="A summary of the visit-level metadata.",
         name="preliminary_visit_summary",
         storageClass="ExposureCatalog",
-        dimensions=("instrument", "visit")
+        dimensions=("instrument", "visit"),
     )
     quiverKernelPlot = connectionTypes.Output(
         doc="A postagestamp composite image of the tract.",
@@ -70,12 +71,12 @@ class MakeDiffimKernelQuiverPlotVisitConfig(
     )
     color = lsst.pex.config.Field(
         dtype=str,
-        default="blue",
-        doc="Color for quiver arrows",
+        default="hsv",
+        doc="Color map for quiver arrows",
     )
     width = lsst.pex.config.Field(
         dtype=float,
-        default=0.0004,
+        default=0.0005,
         doc="Width of quiver arrows",
     )
     scale = lsst.pex.config.Field(
@@ -85,7 +86,7 @@ class MakeDiffimKernelQuiverPlotVisitConfig(
     )
     scale_units = lsst.pex.config.Field(
         dtype=str,
-        default='xy',
+        default="xy",
         doc="Scale units for quiver arrows",
     )
     qKeySize = lsst.pex.config.Field(
@@ -133,23 +134,33 @@ class MakeDiffimKernelQuiverPlotVisitTask(pipeBase.PipelineTask):
 
         quiverConf = {
             "pivot": self.config.pivot,
-            "color": self.config.color,
             "width": self.config.width,
             "scale": self.config.scale,
             "scale_units": self.config.scale_units,
         }
+        if self.config.color not in plt.colormaps():
+            quiverConf["color"] = self.config.color
+        else:
+            # Normalize directions to [0, 1] for colormap, covering -pi to pi
+            norm = plt.Normalize(vmin=-np.pi, vmax=np.pi)
+            cmap = plt.get_cmap(self.config.color)
+
         qKeyLabel = f"{self.config.qKeySize} arcsec Kernel offset direction"
         xAxisLabel = "RA (degrees)"
         yAxisLabel = "Dec (degrees)"
+
+        camera = LsstCam().getCamera()
+        detectorColors = {"ITL": "blue", "E2V": "red"}
 
         fig = plt.figure(dpi=300, figsize=(20, 20))
         ax = fig.add_subplot(111)
         dec_avg = []
         for data_ref in spatiallySampledMetrics:
-            detector = data_ref.dataId['detector']
+            detector = data_ref.dataId["detector"]
             data = data_ref.get()
-            dataSelector = np.isfinite(data["psfMatchingKernel_direction"]) & \
-                np.isfinite(data["psfMatchingKernel_length"])
+            dataSelector = np.isfinite(data["psfMatchingKernel_direction"]) & np.isfinite(
+                data["psfMatchingKernel_length"]
+            )
             dataX = np.rad2deg(data["coord_ra"][dataSelector])
             dataY = np.rad2deg(data["coord_dec"][dataSelector])
             dec_avg.append(np.mean(data["coord_dec"][dataSelector]))
@@ -159,33 +170,35 @@ class MakeDiffimKernelQuiverPlotVisitTask(pipeBase.PipelineTask):
             U = dataL * np.cos(dataA)
             V = dataL * np.sin(dataA)
 
-            q = ax.quiver(dataX, dataY, U, V, **quiverConf)
-            draw_detector_outlines(detector, visit_summary, color='red', ax=ax, linewidth=0.5)
+            if "color" in quiverConf:
+                q = ax.quiver(dataX, dataY, U, V, **quiverConf)
+            else:
+                colors = cmap(norm(dataA))
+                q = ax.quiver(dataX, dataY, U, V, color=colors, **quiverConf)
+            color = detectorColors[camera[detector].getPhysicalType()]
+            draw_detector_outlines(detector, visit_summary, color=color, ax=ax, linewidth=0.5)
         dec_avg = np.mean(dec_avg)
         ax.quiverkey(q, 0.8, 0.95, self.config.qKeySize, qKeyLabel, labelpos="E", coordinates="axes")
         ax.invert_xaxis()
         ax.set_xlabel(xAxisLabel)
         ax.set_ylabel(yAxisLabel)
-        ax.set_aspect(1/np.cos(dec_avg))
+        ax.set_aspect(1 / np.cos(dec_avg))
 
         plt.subplots_adjust(wspace=0.0, hspace=0.0, right=0.85)
 
-        # # add general plot info
-        # if plotInfo is not None:
-        #     fig = addPlotInfo(fig, plotInfo)
         fig.canvas.draw()
         return fig
 
 
-def draw_detector_outlines(detector, visit_summary, color='red', ax=None, linewidth=1):
-    row = np.where(visit_summary['id'] == detector)[0][0]
-    ra_corners = list(visit_summary['raCorners'][row])
-    dec_corners = list(visit_summary['decCorners'][row])
+def draw_detector_outlines(detector, visit_summary, color="red", ax=None, linewidth=1):
+    row = np.where(visit_summary["id"] == detector)[0][0]
+    ra_corners = list(visit_summary["raCorners"][row])
+    dec_corners = list(visit_summary["decCorners"][row])
     ra_center = np.mean(ra_corners)
     dec_center = np.mean(dec_corners)
 
     ra_corners.append(ra_corners[0])  # repeat to close loop
     dec_corners.append(dec_corners[0])  # repeat to close loop
 
-    ax.plot(ra_corners, dec_corners, '--', color=color, alpha=0.9, linewidth=linewidth)
+    ax.plot(ra_corners, dec_corners, "--", color=color, alpha=0.9, linewidth=linewidth)
     ax.text(ra_center, dec_center, detector, alpha=1)

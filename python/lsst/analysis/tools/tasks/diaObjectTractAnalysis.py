b4e071207fbfb4f88139643ed40530a6fb20ac57
Meredith Rawls <mrawls@uw.edu>
2025-11-04 15:13:33 -0800

    Start of new dia_object plot (WIP)

---
+++ python/lsst/analysis/tools/tasks/diaObjectTractAnalysis.py
@@ -0,0 +1,56 @@
+# This file is part of analysis_tools.
+#
+# Developed for the LSST Data Management System.
+# This product includes software developed by the LSST Project
+# (https://www.lsst.org).
+# See the COPYRIGHT file at the top-level directory of this distribution
+# for details of code ownership.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+
+from __future__ import annotations
+
+__all__ = (
+    "DiaObjectTractAnalysisConnections",
+    "DiaObjectTractAnalysisConfig",
+    "DiaObjectTractAnalysisTask",
+)
+
+from lsst.pipe.base import connectionTypes as ct
+
+from ..interfaces import AnalysisBaseConfig, AnalysisBaseConnections, AnalysisPipelineTask
+
+
+class DiaObjectTractAnalysisConnections(
+    AnalysisBaseConnections,
+    dimensions=("tract",),
+):
+    data = ct.Input(
+        doc="Tract-based DiaObject table to load from the butler",
+        name="dia_object",
+        storageClass="ArrowAstropy",
+        deferLoad=True,
+        dimensions=("tract",),
+    )
+
+
+class DiaObjectTractAnalysisConfig(
+    AnalysisBaseConfig, pipelineConnections=DiaObjectTractAnalysisConnections
+):
+    pass
+
+
+class DiaObjectTractAnalysisTask(AnalysisPipelineTask):
+    ConfigClass = DiaObjectTractAnalysisConfig
+    _DefaultName = "DiaObjectTractAnalysis"
